a4e5c387e2e8c8d66e6d7852f0a63e8429fbc581
15thanh09 <15thanh09@gmail.com>
2023-03-05 20:50:25 +0700

    test

--- ssh.py
+++ ssh.py
@@ -1,6 +1,23 @@
 import paramiko
+host = '27.64.30.83'
+username = 'thanh'
+password = '1509'
+port=6767
+# create an SSH client object
 ssh = paramiko.SSHClient()
+
+# automatically add the server's host key
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect("27.64.30.83", username="thanh", password="1509", port=6767)
-ssh.exec_command("cd ~/Desktop/SWT/testswt")
-ssh.exec_command("git pull")
+
+# connect to the server
+ssh.connect(host, username='thanh'
+, password=password, port=port)
+
+# execute a command
+ssh.exec_command('cd ~/Desktop/SWT/testswt ls')
+stdin, stdout, stderr = ssh.exec_command('cd ~/Desktop/SWT/testswt\ngit pull')
+# read the output
+output = stdout.read().decode('utf-8')
+
+# print the output
+print(output)
